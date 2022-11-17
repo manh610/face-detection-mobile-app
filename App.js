8969b33c3e23a2a5f5eed02fe5aecfea77320849
manh <manhdd162@gmail.com>
2022-11-17 09:46:50 +0700

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
-// The following packages need to be installed using the following commands:
-// expo install expo-camera
-// expo install expo-media-library
-// expo install expo-sharing
-// expo install expo-av
-
-import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, TouchableOpacity, Image } from 'react-native';
 import { useEffect, useState, useRef } from 'react';
 import { Camera } from 'expo-camera';
 import { Video } from 'expo-av';
@@ -12,100 +6,250 @@ import { shareAsync } from 'expo-sharing';
 import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
-  let cameraRef = useRef();
-  const [hasCameraPermission, setHasCameraPermission] = useState();
-  const [hasMicrophonePermission, setHasMicrophonePermission] = useState();
-  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
-  const [isRecording, setIsRecording] = useState(false);
-  const [video, setVideo] = useState();
 
-  useEffect(() => {
-    (async () => {
-      const cameraPermission = await Camera.requestCameraPermissionsAsync();
-      const microphonePermission = await Camera.requestMicrophonePermissionsAsync();
-      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+	const [isCam, setIsCam] = useState(false);
 
-      setHasCameraPermission(cameraPermission.status === "granted");
-      setHasMicrophonePermission(microphonePermission.status === "granted");
-      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
-    })();
-  }, []);
+    let cameraRef = useRef();
+    const [hasCameraPermission, setHasCameraPermission] = useState();
+    const [hasMicrophonePermission, setHasMicrophonePermission] = useState();
+    const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+    const [isRecording, setIsRecording] = useState(false);
+    const [video, setVideo] = useState();
+	const [photo, setPhoto] = useState();
 
-  if (hasCameraPermission === undefined || hasMicrophonePermission === undefined) {
-    return <Text>Requestion permissions...</Text>
-  } else if (!hasCameraPermission) {
-    return <Text>Permission for camera not granted.</Text>
-  }
+    useEffect(() => {
+		if ( isCam ) {
+			(async () => {
+				const cameraPermission = await Camera.requestCameraPermissionsAsync();
+				const microphonePermission = await Camera.requestMicrophonePermissionsAsync();
+				const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+	
+				setHasCameraPermission(cameraPermission.status === "granted");
+				setHasMicrophonePermission(microphonePermission.status === "granted");
+				setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+			})();
+		}
+    }, [isCam]);
 
-  let recordVideo = () => {
-    setIsRecording(true);
-    let options = {
-      quality: "1080p",
-      maxDuration: 60,
-      mute: false
+    let recordVideo = () => {
+		setIsRecording(true);
+		let options = {
+			quality: "1080p",
+			maxDuration: 60,
+			mute: false
+		};
+
+		cameraRef.current.recordAsync(options).then((recordedVideo) => {
+			setVideo(recordedVideo);
+			setIsRecording(false);
+		});
     };
 
-    cameraRef.current.recordAsync(options).then((recordedVideo) => {
-      setVideo(recordedVideo);
-      setIsRecording(false);
-    });
-  };
-
-  let stopRecording = () => {
-    setIsRecording(false);
-    cameraRef.current.stopRecording();
-  };
-
-  if (video) {
-    let shareVideo = () => {
-      shareAsync(video.uri).then(() => {
-        setVideo(undefined);
-      });
+    let stopRecording = () => {
+		setIsRecording(false);
+		cameraRef.current.stopRecording();
     };
 
-    let saveVideo = () => {
-      MediaLibrary.saveToLibraryAsync(video.uri).then(() => {
-        setVideo(undefined);
-      });
-    };
+	let takePhoto = async () => {
+		const options = { quality: 0.5, base64: true, skipProcessing: true };
+		await cameraRef.current.takePictureAsync().then((photo) => {
+			console.log(photo.uri);
+			setPhoto(photo);
+		})
+		// await cameraRef.current.pausePreview();
+	}
 
-    return (
-      <SafeAreaView style={styles.container}>
-        <Video
-          style={styles.video}
-          source={{uri: video.uri}}
-          useNativeControls
-          resizeMode='contain'
-          isLooping
-        />
-        <Button title="Share" onPress={shareVideo} />
-        {hasMediaLibraryPermission ? <Button title="Save" onPress={saveVideo} /> : undefined}
-        <Button title="Discard" onPress={() => setVideo(undefined)} />
-      </SafeAreaView>
+	const requestDetect = () => {
+		console.log('request to server to detect')
+	}
+
+	if ( photo ) {
+
+		let savePhoto = async () => {
+			await MediaLibrary.saveToLibraryAsync(photo.uri);
+		};
+
+		return (
+			<SafeAreaView style={styles.containerTmp}>
+				<Image style={{height: '95%', width: '100%'}} source={{uri: photo.uri}} />
+				<View style={styles.btnAfter}>
+					{
+						hasMediaLibraryPermission && <TouchableOpacity style={styles.btnSave} onPress={() => savePhoto()}>
+							<Text>SAVE</Text>
+						</TouchableOpacity>
+					}
+					<TouchableOpacity style={styles.btnDetect} onPress={() => requestDetect()}>
+						<Text>DETECT</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.btnDiscard} onPress={() => setPhoto(undefined)}>
+						<Text>DISCARD</Text>
+					</TouchableOpacity>
+				</View>
+			</SafeAreaView>
+		)
+	}
+
+    if (video) {
+
+		let saveVideo = () => {
+			MediaLibrary.saveToLibraryAsync(video.uri).then(() => {
+				setVideo(undefined);
+			});
+		};
+
+		return (
+			<SafeAreaView style={styles.containerTmp}>
+				<Video
+					style={styles.video}
+					source={{uri: video.uri}}
+					useNativeControls
+					resizeMode='contain'
+					isLooping
+				/>
+				<View style={styles.btnAfter}>
+					{
+						hasMediaLibraryPermission && <TouchableOpacity style={styles.btnSave} onPress={() => saveVideo()}>
+							<Text>SAVE</Text>
+						</TouchableOpacity>
+					}
+					<TouchableOpacity style={styles.btnDetect} onPress={() => requestDetect()}>
+						<Text>DETECT</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.btnDiscard} onPress={() => setVideo(undefined)}>
+						<Text>DISCARD</Text>
+					</TouchableOpacity>
+				</View>
+			</SafeAreaView>
+		);
+    }
+
+	const getCam = () => {
+		setIsCam(true)
+	}
+
+	const back = () => {
+		if(isRecording) {
+			stopRecording();
+			setVideo(undefined)
+		}
+		setIsCam(false)
+	}
+
+	const renderCamera = () => {
+		return (
+			isCam && (<Camera style={styles.container} ref={cameraRef}>
+				<View style={styles.btnVideo}>
+					<TouchableOpacity onPress={isRecording ? stopRecording : recordVideo} style={styles.btnRecord}>
+						<Text>{isRecording ? "Stop Recording" : "Record Video"}</Text>
+					</TouchableOpacity>
+					{
+						!isRecording && (
+							<TouchableOpacity onPress={takePhoto} style={styles.btnTakePhoto}>
+								<Text>Take photo</Text>
+							</TouchableOpacity>
+						)
+					}
+					<TouchableOpacity onPress={() => back()} style={styles.btnBack}>
+						<Text>BACK</Text>
+					</TouchableOpacity>
+				</View>
+			</Camera> )
+		);
+	}
+
+	return (
+		<View style={styles.content}>
+			{!isCam &&<View style={styles.header}></View>}
+			{!isCam && (
+				<TouchableOpacity onPress={() => getCam()} style={styles.btnGetCam}>
+					<Text style={{color: '#FFFFFF', textAlign:'center'}}>GET CAMERA</Text>
+				</TouchableOpacity>
+			)}
+			{renderCamera()}
+		</View>
     );
+    
   }
 
-  return (
-    <Camera style={styles.container} ref={cameraRef}>
-      <View style={styles.buttonContainer}>
-        <Button title={isRecording ? "Stop Recording" : "Record Video"} onPress={isRecording ? stopRecording : recordVideo} />
-      </View>
-    </Camera>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
-    backgroundColor: "#fff",
-    alignSelf: "flex-end"
-  },
-  video: {
-    flex: 1,
-    alignSelf: "stretch"
-  }
+  const styles = StyleSheet.create({
+	content: {
+		display: 'flex',
+		// alignItems: 'center',
+		// justifyContent: 'center'
+	},	
+	header: {
+		height: 100,
+	},	
+    container: {
+		// flex: 1,
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: '100%'
+    },
+	containerTmp: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+    },
+	btnGetCam: {
+		backgroundColor: '#FF5A80',
+		width: '30%',
+		padding: 10,
+		alignSelf:'center',
+		borderRadius: 10
+	},	
+    buttonContainer: {
+		backgroundColor: "gray",
+		alignSelf: "flex-end"
+	},
+    video: {
+		flex: 1,
+		alignSelf: "stretch"
+    },
+	btnVideo: {
+		display: 'flex',
+		flexDirection: 'row',
+		marginTop: '170%',
+		justifyContent: 'space-between'
+	},
+	btnRecord: {
+		backgroundColor: '#FF5A80',
+		padding: 15,
+		borderRadius: 15
+	}, 
+	btnTakePhoto: {
+		marginLeft: 10,
+		marginRight: 10,
+		backgroundColor: '#FF5A80',
+		padding: 15,
+		borderRadius: 15
+	},
+	btnBack: {
+		backgroundColor: 'gray',
+		padding: 15,
+		borderRadius: 15
+	},
+	btnAfter: {
+		display: 'flex',
+		flexDirection: 'row',
+		width: '90%',
+		justifyContent: 'space-around',
+		marginBottom: '5%'
+	},
+	btnSave: {
+		backgroundColor: '#5db8f0',
+		padding: 10,
+		borderRadius: 10
+	},
+	btnDetect: {
+		backgroundColor: '#FF5A80',
+		padding: 10,
+		borderRadius: 10
+	},
+	btnDiscard: {
+		backgroundColor: '#a1abad',
+		padding: 10,
+		borderRadius: 10
+	}
 });
